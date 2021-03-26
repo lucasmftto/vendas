0775255d38316b7aaf50f1acdcb50b974d7e915e
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-03-25 22:36:57 -0300

    Tratamento de erros

--- src/main/java/br/com/lucasmftto/rest/ClienteController.java
+++ src/main/java/br/com/lucasmftto/rest/ClienteController.java
@@ -1,12 +1,19 @@
 package br.com.lucasmftto.rest;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.lucasmftto.model.entity.Cliente;
 import br.com.lucasmftto.model.repository.ClienteRepositoty;
@@ -14,14 +21,44 @@ import br.com.lucasmftto.model.repository.ClienteRepositoty;
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteRepositoty repositoty;
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(@RequestBody Cliente cliente) {
+	public Cliente salvar(@RequestBody @Valid Cliente cliente) {
 		return repositoty.save(cliente);
+	}
+
+	@GetMapping("{id}")
+	public Cliente findById(@PathVariable Integer id) {
+		return repositoty.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		repositoty.findById(id)
+			.map( cliente -> {
+				repositoty.delete(cliente);
+				return Void.TYPE;
+			})
+			
+		.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+	
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@PathVariable Integer id, @Valid @RequestBody Cliente newCliente) {
+		repositoty.findById(id)
+		.map( cliente -> {
+			newCliente.setId(cliente.getId());
+			repositoty.save(newCliente);
+			return Void.TYPE;
+		})
+		
+	.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}
 
 }
