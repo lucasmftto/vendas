c1f2553f93ebbf1277b9bc09d5260c8f4c5c9742
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-04-21 17:08:10 -0300

    Criacao de login com Jwt

--- src/main/java/br/com/lucasmftto/model/repository/UsuarioRepository.java
+++ src/main/java/br/com/lucasmftto/model/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package br.com.lucasmftto.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lucasmftto.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+	
+	Optional<Usuario> findByUsername(String username);
 }
