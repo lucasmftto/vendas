52dd4c0e73bc7c4bcfbbd181c513fb8348ad956a
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-09-07 00:51:34 -0300

    Estudo JWT, recuperar authorities from token

--- src/main/java/br/com/lucasmftto/rest/ClienteController.java
+++ src/main/java/br/com/lucasmftto/rest/ClienteController.java
@@ -1,18 +1,25 @@
 package br.com.lucasmftto.rest;
 
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +27,10 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.com.lucasmftto.model.entity.Cliente;
 import br.com.lucasmftto.model.repository.ClienteRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -27,10 +38,33 @@ public class ClienteController {
 
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Value("${security.jwt.signing-key}")
+	private String signingKey;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(@RequestBody @Valid Cliente cliente) {
+	public Cliente salvar(@RequestBody @Valid Cliente cliente, @RequestHeader("Authorization") String token) {
+		
+		String newToken = token.substring(7, token.length());
+		 Claims claims2 = null;
+		  try {
+		    claims2 = Jwts.parser()
+		        .setSigningKey(signingKey.getBytes(Charset.forName("UTF-8")))
+		        .parseClaimsJws(newToken)
+		        .getBody();
+		  } catch (Exception e) {
+		    e.printStackTrace();
+		  }
+		
+		  final List<GrantedAuthority> authorities =
+				  Arrays.stream(claims2.get("authorities").toString().split(","))
+                  .map(SimpleGrantedAuthority::new)
+                  .collect(Collectors.toList());
+		  
+		  for (GrantedAuthority grantedAuthority : authorities) {
+			  System.out.println(grantedAuthority.getAuthority());
+		}	 
 		return repository.save(cliente);
 	}
 
